cef9a0b4f24edf6202e6fce0fbf1a892270210fe
yuki-inaho <yoshikawa@inaho.co>
2022-12-13 20:47:06 +0900

    Add lines to dump estimated texts

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import time
 import whisper
 from pathlib import Path
+from typing import Iterator, TextIO
+
+
+def write_txt(transcript: Iterator[dict], file: TextIO):
+    for segment in transcript:
+        print(segment["text"].strip(), file=file, flush=True)
+
 
 """ Set the audio file path and weight path
 """
@@ -10,6 +17,8 @@ audio_path = str(audio_dir_pathlib.joinpath(audio_name))
 
 weights_dir_pathlib = Path("./whisper")
 weights_name = "large-v2.pt"
+
+output_txt_path = Path(audio_name).stem + ".txt"
 
 """ Loading the model
 """
@@ -25,6 +34,12 @@ print(f"Loading the whisper model is done. Elapsed: {end-start}")
 
 """ Run transcription (with GPU)
 """
-result = model.transcribe(audio_path, verbose=True, language="japanese", beam_size=5, fp16=True, without_timestamps=True)
+start = time.time()
+result = model.transcribe(audio_path, verbose=True, language="japanese", beam_size=5, fp16=True, without_timestamps=False)
+end = time.time()
 print(f"Transcription is done, Elapsed: {end-start}")
-print(result["text"])
+
+""" Dump the text file
+"""
+with open(output_txt_path, "w", encoding="utf-8") as txt:
+    write_txt(result["segments"], file=txt)
